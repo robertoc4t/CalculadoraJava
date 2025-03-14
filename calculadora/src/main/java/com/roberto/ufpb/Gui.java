cec4e8751f0d427fcfa835bd876811ccf1840652
robertoc4t <robertsilva1229@gmail.com>
2025-03-14 18:36:42 -0300

    GUI integrada

---
+++ calculadora/src/main/java/com/roberto/ufpb/Gui.java
@@ -0,0 +1,13 @@
+package com.roberto.ufpb;
+import javax.swing.JFrame;
+
+public class Gui extends JFrame{
+
+    public Gui(){
+        setTitle("Calculadora");
+        setSize(350, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+}
--- calculadora/src/main/java/com/roberto/ufpb/Main.java
+++ calculadora/src/main/java/com/roberto/ufpb/Main.java
@@ -4,6 +4,7 @@ package com.roberto.ufpb;
 public class Main {
     public static void main(String[] args) {
         
+        Gui gui = new Gui();
         
     }
 }
