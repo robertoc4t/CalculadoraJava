6f2e9bca85fe1792cb00b424edf70fb2062b6ff5
robertoc4t <robertsilva1229@gmail.com>
2025-03-14 18:52:55 -0300

    Visor adicionado

--- calculadora/src/main/java/com/roberto/ufpb/Gui.java
+++ calculadora/src/main/java/com/roberto/ufpb/Gui.java
@@ -1,5 +1,9 @@
 package com.roberto.ufpb;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Gui extends JFrame{
 
@@ -8,6 +12,17 @@ public class Gui extends JFrame{
         setSize(350, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+        
+        setLayout(new BorderLayout());
+
+
+
+        JPanel visor = new JPanel();
+        visor.setPreferredSize(new java.awt.Dimension(350, 70));
+        visor.setBackground(Color.BLUE);
+        visor.setVisible(true);
+        add(visor, BorderLayout.NORTH);
     }
     
 }
