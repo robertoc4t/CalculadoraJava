97f3e7b9b71a631654111f944f231275df24421d
robertoc4t <robertsilva1229@gmail.com>
2025-03-15 19:35:48 -0300

    Numeric buttons added

--- calculadora/src/main/java/com/roberto/ufpb/Button.java
+++ calculadora/src/main/java/com/roberto/ufpb/Button.java
@@ -1,18 +1,26 @@
 package com.roberto.ufpb;
 
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JButton;
 
 public class Button extends JButton{
     private int value;
+
     
     public Button(int value){
 
-        setSize(10,10);
+        setPreferredSize(new Dimension(100, 50));
         setVisible(true);
         setBackground(Color.BLACK);
+        setText(String.valueOf(value));
+        setBackground(Color.white);
 
         this.value = value;
+    }
+
+    public void setDimension(int a, int b){
+        setPreferredSize(new Dimension(a, b));
     }
 }
--- calculadora/src/main/java/com/roberto/ufpb/Gui.java
+++ calculadora/src/main/java/com/roberto/ufpb/Gui.java
@@ -1,31 +1,67 @@
 package com.roberto.ufpb;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Gui extends JFrame{
+public class Gui extends JFrame {
 
-    public Gui(){
+    public Gui() {
         setTitle("Calculadora");
-        setSize(350, 500);
+        setSize(335, 470);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-
-        
         setLayout(new BorderLayout());
 
 
 
         JPanel visor = new JPanel();
-        visor.setPreferredSize(new java.awt.Dimension(350, 70));
+        visor.setPreferredSize(new Dimension(350, 70));
         visor.setBackground(Color.BLUE);
-        visor.setVisible(true);
         add(visor, BorderLayout.NORTH);
 
-        Button n1 = new Button(1);
-        add(n1, BorderLayout.SOUTH);
+        // Painel para os botões
+        JPanel painelBotoes = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        Button n1 = new Button(1);      
+        painelBotoes.add(n1);
+
+        Button n2 = new Button(2);
+        painelBotoes.add(n2);
+
+        Button n3 = new Button(3);
+        painelBotoes.add(n3);
+
+        Button n4 = new Button(4);
+        painelBotoes.add(n4);
+
+        Button n5 = new Button(5);
+        painelBotoes.add(n5);
+
+        Button n6 = new Button(6);
+        painelBotoes.add(n6);
+
+        Button n7 = new Button(7);
+        painelBotoes.add(n7);
+
+        Button n8 = new Button(8);
+        painelBotoes.add(n8);
+
+        Button n9 = new Button(9);
+        painelBotoes.add(n9);
+
+        Button n0 = new Button(0);
+        n0.setDimension(310, 70);
+        painelBotoes.add(n0);
+
+
+
+        // Adiciona o painel de botões abaixo do visor
+        add(painelBotoes, BorderLayout.CENTER);
+
+        setVisible(true);
     }
-    
+
 }
