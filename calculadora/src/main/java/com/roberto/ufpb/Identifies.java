e23428441ae45fe17d243a06b091154481fb0e4c
robertoc4t <robertsilva1229@gmail.com>
2025-03-16 22:58:56 -0300

    Identifies added

---
+++ calculadora/src/main/java/com/roberto/ufpb/Identifies.java
@@ -0,0 +1,4 @@
+package com.roberto.ufpb;
+public class Identifies{
+    
+}
--- calculadora/src/main/java/com/roberto/ufpb/Mananger.java
+++ calculadora/src/main/java/com/roberto/ufpb/Mananger.java
@@ -1,5 +1,14 @@
 package com.roberto.ufpb;
 
+import java.awt.event.ActionEvent;
+
 public class Mananger {
+    private int value1;
+    private int value2;
+    private String valueOperation;
     
+
+    public void actionSum(ActionEvent e){
+        
+    }
 }
